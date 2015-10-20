4239753f9adc4aff09f62b575b42471df867ea1e
Zonghan Wu <a476678244@163.com>
2015-10-20 09:41:18 +0800

    'override

---
+++ src/main/java/learnHystrix/variargs/Main.java
@@ -0,0 +1,12 @@
+package learnHystrix.variargs;
+
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int[] digits = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 4 };
+    System.out.println(Arrays.asList(digits));
+  }
+
+}
