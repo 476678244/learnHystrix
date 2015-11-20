ac32fb1a33d7841760fee715988e990731301b9b
Zonghan Wu <a476678244@163.com>
2015-11-20 16:29:34 +0800

    Executor

---
+++ src/main/java/learnHystrix/concurrency/snapshot/ObservableSet.java
@@ -0,0 +1,73 @@
+package learnHystrix.concurrency.snapshot;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import learnHystrix.concurrency.ForwardingSet;
+import learnHystrix.concurrency.snapshot.SetObserver;
+
+//Broken - invokes alien method from synchronized block!
+public class ObservableSet<E> extends ForwardingSet<E> {
+
+  public static void main(String[] args) {
+    ObservableSet<Integer> set = new ObservableSet<Integer>(
+        new HashSet<Integer>());
+    set.addObserver(new SetObserver<Integer>() {
+      @Override
+      public void added(ObservableSet<Integer> s, Integer e) {
+        System.out.println(e);
+        if (e == 23)
+          s.removeObserver(this);
+      }
+    });
+    for (int i = 0; i < 100; i++)
+      set.add(i);
+  }
+
+  public ObservableSet(Set<E> set) {
+    super(set);
+  }
+
+  private final List<SetObserver<E>> observers = new ArrayList<SetObserver<E>>();
+
+  public void addObserver(SetObserver<E> observer) {
+    synchronized (observers) {
+      observers.add(observer);
+    }
+  }
+
+  public boolean removeObserver(SetObserver<E> observer) {
+    synchronized (observers) {
+      return observers.remove(observer);
+    }
+  }
+
+  // Alien method moved outside of synchronized block - open calls
+  private void notifyElementAdded(E element) {
+    List<SetObserver<E>> snapshot = null;
+    synchronized (observers) {
+      snapshot = new ArrayList<SetObserver<E>>(observers);
+    }
+    for (SetObserver<E> observer : snapshot)
+      observer.added(this, element);
+  }
+
+  @Override
+  public boolean add(E element) {
+    boolean added = super.add(element);
+    if (added)
+      notifyElementAdded(element);
+    return added;
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    boolean result = false;
+    for (E element : c)
+      result |= add(element); // calls notifyElementAdded
+    return result;
+  }
+}
---
+++ src/main/java/learnHystrix/concurrency/snapshot/SetObserver.java
@@ -0,0 +1,6 @@
+package learnHystrix.concurrency.snapshot;
+
+public interface SetObserver<E> {
+  // Invoked when an element is added to the observable set
+  void added(ObservableSet<E> set, E element);
+}
